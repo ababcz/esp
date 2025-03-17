08cfebfb886700c8348be03cfd5b91476463d286
ababcz <63705958+ababcz@users.noreply.github.com>
2025-03-17 23:06:48 +0100

    a

--- c6.py
+++ c6.py
@@ -1,1 +1,44 @@
+from machine import Pin
+from neopixel import NeoPixel
+from time import sleep
+
+# Nastavení RGB LED na GPIO 8 (1 LED)
+pin_rgb = Pin(8, Pin.OUT)
+np = NeoPixel(pin_rgb, 1)
+
+# Definice barev
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+OFF = (0, 0, 0)
+
+def blikani_majaku(pocet_opakovani=10):
+    for _ in range(pocet_opakovani):
+        # Dvakrát rychle červená
+        for _ in range(2):
+            np[0] = RED
+            np.write()
+            sleep(0.1)
+            np[0] = OFF
+            np.write()
+            sleep(0.1)
+
+        sleep(0.2)
+
+        # Dvakrát rychle modrá
+        for _ in range(2):
+            np[0] = BLUE
+            np.write()
+            sleep(0.1)
+            np[0] = OFF
+            np.write()
+            sleep(0.1)
+
+        sleep(0.2)
+
+    # Po ukončení zhasni LED
+    np[0] = OFF
+    np.write()
+
+# Spuštění blikání majáku s 10 opakováními
+blikani_majaku()
 
