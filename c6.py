e4894023c6cd72cb5c792c574b6edcbd2a398712
ababcz <63705958+ababcz@users.noreply.github.com>
2025-04-03 22:06:36 +0200

    Update c6.py

--- c6.py
+++ c6.py
@@ -14,7 +14,7 @@ OFF = (0, 0, 0)
 def blikani_majaku(pocet_opakovani=10):
     for _ in range(pocet_opakovani):
         # Dvakrát rychle červená
-        for _ in range(3):
+        for _ in range(4):
             np[0] = RED
             np.write()
             sleep(0.1)
